0e63e7b467517aaf99c47e9303ac191f51bb9e5a
oumar <oumardialo98@qq.com>
2021-12-08 10:37:31 +0800

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import installElementPlus from './plugins/element'
 
-Vue.config.productionTip = false
+import './assets/style/style.css'
+import './assets/boostrap/css/bootstrap.min.css'
+import './assets/boostrap/css/bootstrap-icons.css'
+import 'nprogress/nprogress.css'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+
+const app = createApp(App)
+installElementPlus(app)
+
+app.use(store).use(router).mount('#app')
